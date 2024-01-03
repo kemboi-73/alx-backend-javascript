f1b118529c3c111c5d7aa6932af6c705bda0fa86
kemboi-73 <kemboimichael52@gmail.com>
2024-01-03 07:22:52 +0000

    changing func to instantiate variable

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,3 @@
-// 0-constants.js
-
-// Use const in taskFirst
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
@@ -10,11 +7,9 @@ export function getLast() {
   return ' is okay';
 }
 
-// Use let in taskNext
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
 }
-
